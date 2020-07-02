108fc001db396bc1ed6738c69c1cdaf783e4758c
Dimas Achmad <dimasachmadnugraha@gmail.com>
2020-07-02 14:02:01 +0700

    edit templating blade

--- Belajar_Templating/routes/web.php
+++ Belajar_Templating/routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/master', function(){
+    return view('adminLTE.master');
+});
+
+Route::get('/', function(){
+    return view('data.tabel');
+});
+
+Route::get('/data-tables', function(){
+    return view('data.tabel-data');
 });
